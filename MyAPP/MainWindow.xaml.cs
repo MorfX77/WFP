d5ca9b8ef39b3c091f42ebf7dd9a8c7623ef2882
MorfX77 <144059142+MorfX77@users.noreply.github.com>
2024-12-17 11:43:25 +0300

    Add files via upload

---
+++ MyAPP/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAPP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            author aut = new author();
+            aut.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Btn_monitoring(object sender, RoutedEventArgs e)
+        {
+            monitoring mon = new monitoring();
+            mon.Show();
+            this.Close();
+        }
+    }
+}
